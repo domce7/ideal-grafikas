be1215c63b6c68930fea8b793761df576437e880
Domantas <domantas.karpinskas@gmail.com>
2022-07-28 22:46:08 +0300

    Bug fixes

--- SPA/excel_to_ics/urls.py
+++ SPA/excel_to_ics/urls.py
@@ -4,8 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.homepage.as_view(), name='home'),
-    path('upload/', views.upload, name='upload'),
+    path('', views.upload, name='upload'),
     path('upload_list/', views.upload_list, name='upload_list'),
     path('after-upload/', views.after_upload, name='after_upload'),
 ]
